3f70fdf5bfe97c785db36e6e9c9ff28d4f06cddc
ikram3031 <mdikram.3031@gmail.com>
2024-04-13 22:20:33 +0600

    AuthCallbackPage

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,14 +1,12 @@
-import { useCreateMyUser } from "@/api/MyUserApi";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
     children: React.ReactNode
 }
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
-    // const navigate = useNavigate();
-    const { createUser, isLoading } = useCreateMyUser();
+    const navigate = useNavigate();
     
     const domain = import.meta.env.VITE_AUTH0_DOMAIN;
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
@@ -19,10 +17,7 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
     }
     
     const onRedirectCallback = (appState?: AppState, user?: User) => {
-        // navigate(appState?.returnTo || "/auth-callback");
-        if(user?.sub && user?.email) {
-            createUser({ auth0Id: user.sub, email: user.email })
-        }
+        navigate(appState?.returnTo || "/auth-callback");        
     };
     
     return (
