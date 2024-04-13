c58240830c6f7ad0cfa0c5d35ad7b791c686d4c2
ikram3031 <mdikram.3031@gmail.com>
2024-04-13 22:47:06 +0600

    03:08

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response} from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-import myUserRoute from './routes/myUserRoute';
+import MyUserRoute from './routes/myUserRoute';
 
 mongoose
     .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json())
 app.use(cors())
 
-app.use("/api/my/user", myUserRoute);
+app.use("/api/my/user", MyUserRoute);
 
 app.listen(7000, () => {
     console.log('Server started on localhost:7000')
