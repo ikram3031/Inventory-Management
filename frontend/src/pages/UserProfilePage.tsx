1a7c3610d299bcdf7cd229499749db41238ad69c
ikram3031 <mdikram.3031@gmail.com>
2024-04-27 14:46:47 +0600

    Update Profile working

--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -1,8 +1,16 @@
+import { useUpdateMyUser } from "@/api/MyUserApi";
 import UserProfileForm from "@/forms/user-profile-form/UserProfileForm";
 
 
-export default function UserProfilePage() {
-  return (
-    <UserProfileForm/>
-  )
+const UserProfilePage = () =>{
+    const { updateUser, isLoading } = useUpdateMyUser();
+    
+    return (
+        <UserProfileForm 
+            onSave={updateUser} 
+            isLoading={isLoading}
+        />
+    )
 }
+
+export default UserProfilePage;
