598523a250309e0debec7e17c6253bb91c8e51ae
ikram3031 <mdikram.3031@gmail.com>
2024-05-04 22:46:02 +0600

    get user intregated and default value in form

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -1,20 +1,20 @@
 import { Request, Response } from "express";
 import User from "../models/user";
 
-const getCurrentUser = async (req: Request, res:Response) => {
-    try{
-        const currentUser = await User.findOne({ _id: req.userId});
-        if(!currentUser){
-            return res.status(404).json({ message: "User not found" });
-        }
-        res.status(200).json({ message: "User not found"});
-        
-        res.json(currentUser)
-    }catch(error){
-        console.log(error);
-        res.status(500).json({ message: "Error getting user" });
+
+const getCurrentUser = async (req: Request, res: Response) => {
+    try {
+      const currentUser = await User.findOne({ _id: req.userId });
+      if (!currentUser) {
+        return res.status(404).json({ message: "User not found" });
+      }
+  
+      res.json(currentUser);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: "Something went wrong" });
     }
-}
+};
 
 const createCurrentUser = async (req: Request, res:Response) => {
     
