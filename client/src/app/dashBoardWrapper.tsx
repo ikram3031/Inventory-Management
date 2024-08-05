6cd95c031fb1e1abddef0858c28b17d17b76d31a
ikram3031 <mdikram.3031@gmail.com>
2024-08-05 22:28:52 +0600

    24:47

---
+++ client/src/app/dashBoardWrapper.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const DashBoardWrapper = ({children}: {children:React.ReactNode}) => {
+  return (
+    <div>DashBoardWrapper</div>
+  )
+}
+
+export default DashBoardWrapper
---
+++ client/src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type Props = {}
+
+function Dashbboard({}: Props) {
+  return (
+    <div>Dashbboard</div>
+  )
+}
+
+export default Dashbboard
