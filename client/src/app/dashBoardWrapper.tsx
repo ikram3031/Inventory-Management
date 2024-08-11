9fd3f30789184a90a3f04a148f197b02531103e4
ikram3031 <mdikram.3031@gmail.com>
2024-08-11 22:20:23 +0600

    navbar , sidebar

--- client/src/app/dashBoardWrapper.tsx
+++ client/src/app/dashBoardWrapper.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './{components}/Navbar';
+import Sidebar from './{components}/Sidebar';
 
 type Props = {}
 
 const DashBoardWrapper = ({children}: {children:React.ReactNode}) => {
   return (
-    <div>DashBoardWrapper</div>
+    <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
+      <Sidebar />
+      <main className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24`}>
+        <Navbar />
+        {children}
+      </main>
+    </div>
   )
 }
 
--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import DashBoardWrapper from "./dashBoardWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <DashbboardWrapper>{children}</DashbboardWrapper>
+        <DashBoardWrapper>{children}</DashBoardWrapper>
         </body>
     </html>
   );
